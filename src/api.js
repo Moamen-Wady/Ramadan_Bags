fb3b73c7600a9d7d293e9291b87bafb634c33c74
Moamen-Wady <moamen.wady@gmail.com>
2025-02-15 19:51:33 +0200

    Final

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   withCredentials: true,
-  // baseURL: "https://5c-back.vercel.app/",
-  baseURL: "http://localhost:3005",
+  baseURL: "https://rbags-back.vercel.app/",
+  // baseURL: "http://localhost:3005",
 });
 
 export const isCancel = (err) => axios.isCancel(err);
